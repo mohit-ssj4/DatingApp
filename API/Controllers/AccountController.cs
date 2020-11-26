5c493bee0d72d9e123e20ec1db29b2a4fb6e3817
Mohit Singh <mohitsammal1997@gmail.com>
2020-11-26 18:16:22 +0530

    minor changes

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -45,7 +45,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await this._context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await this._context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid username");
 
